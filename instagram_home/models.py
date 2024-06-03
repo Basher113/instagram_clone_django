649e66655ff847178af1e743cc8fbbe714d6ea1f
Basher Kalim <papskalim@gmail.com>
2024-06-03 19:20:26 +0800

    Make database model for posts

--- instagram_home/models.py
+++ instagram_home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.ImageField(upload_to='posts-content')
+    caption = models.TextField()
+
+    def __str__(self):
+        return f"POST: {self.id},  Author: {self.author}"
