985955714be50c844721b2fc6bc2f0bb39aad3a0
Basher Kalim <papskalim@gmail.com>
2024-06-04 15:55:38 +0800

    finish signup html page

--- instagram_home/urls.py
+++ instagram_home/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='instagram-home'),
     path('about/', views.about, name='about'),
-    path('login/', views.login_page, name='login')
+    path('login/', views.login_page, name='login'),
+    path('signup/', views.signup, name='signup')
 ]
--- instagram_home/views.py
+++ instagram_home/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from datetime import datetime
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     context = {
         'posts': Post.objects.all().order_by('-id'),
     }
-    
-
     return render(request, 'instagram_home/instagram.html', context)
 
 def about(request):
@@ -15,5 +14,9 @@ def about(request):
 
 def login_page(request):
     return render(request, 'instagram_home/login_page.html')
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'instagram_home/signup.html', {'form': form})
 
 
