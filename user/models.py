229622ff54df17721e7b5e6ff951be47f92c7e40
Basher Kalim <papskalim@gmail.com>
2024-06-05 13:35:54 +0800

    add auto resize image if big size

--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self):
+        super().save()
+
+        image = Image.open(self.image.path) # get the image
+
+        if image.width > 150 or image.length > 150:
+            max_size = (150, 150) # sizing of max width and length 
+            image.thumbnail(max_size)  # resizing image to max size
+            image.save(self.image.path) # saving image
