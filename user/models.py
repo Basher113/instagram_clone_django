362c707980d5a4ea3c9caf59522c0a1a0e1a766f
Basher Kalim <papskalim@gmail.com>
2024-06-05 14:18:05 +0800

    auto create profile for new user using signals

--- user/models.py
+++ user/models.py
@@ -5,17 +5,17 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default-profile.jpg', upload_to='profile-pics')
-    bio = models.TextField()
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
         image = Image.open(self.image.path) # get the image
 
-        if image.width > 150 or image.length > 150:
+        if image.width > 150 or image.height > 150:
             max_size = (150, 150) # sizing of max width and length 
             image.thumbnail(max_size)  # resizing image to max size
             image.save(self.image.path) # saving image
