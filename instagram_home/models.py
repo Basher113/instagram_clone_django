c7308afe69658b3aaf5965b6f37bc4a03c4c8a80
Basher Kalim <papskalim@gmail.com>
2024-06-06 21:46:09 +0800

    add some feautures to post images in profile when hover

--- instagram_home/models.py
+++ instagram_home/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 from datetime import datetime
 from PIL import Image
 
@@ -8,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.ImageField(upload_to='posts-content')
     caption = models.TextField(blank=True)
-    date_posted = models.DateField(auto_now_add=True, blank=True, null=True)
+    date_posted = models.DateField(default=datetime.now, blank=True, null=True)
 
     def __str__(self):
         return f"POST: {self.id},  Author: {self.author}"
