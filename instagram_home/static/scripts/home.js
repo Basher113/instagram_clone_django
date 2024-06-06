614e56b7b5902505d0643e1965bb88c305417fb6
Basher Kalim <papskalim@gmail.com>
2024-06-07 05:25:48 +0800

    practice jquery

--- instagram_home/static/scripts/home.js
+++ instagram_home/static/scripts/home.js
@@ -1,10 +1,32 @@
-document.querySelector('.js-switch-button').addEventListener('click', () => {
-    document.querySelector('.modal-container').style.display = 'inline'
-    console.log('I am clicked')
+// document.querySelector('.js-switch-button').addEventListener('click', () => {
+//     document.querySelector('.modal-container').style.display = 'inline'
+//     console.log('I am clicked')
     
-    document.querySelector('.js-close-button').addEventListener('click', () => {
-        document.querySelector('.modal-container').style.display = 'none'
+//     document.querySelector('.js-close-button').addEventListener('click', () => {
+//         document.querySelector('.modal-container').style.display = 'none'
+//     })
+// })
+
+$('.js-switch-button').click(() => {
+    $('.modal-container').show()
+
+    $('html, body').css({
+        overflow: 'hidden',
+        height: '100%',
+    });
+
+    $('.js-close-button').click(() => {
+        $('.modal-container').hide()
+
+        $('html, body').css({
+            overflow: 'auto',
+            height: 'auto'
+        });
     })
+
+    
+
 })
 
-console.log('testing')
+
+
