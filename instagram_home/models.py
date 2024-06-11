e7528303d356d14a39dd4e4447dc133745213d02
Basher Kalim <papskalim@gmail.com>
2024-06-11 13:55:07 +0800

    add like and comment model

--- instagram_home/models.py
+++ instagram_home/models.py
@@ -12,3 +12,27 @@ class Post(models.Model):
     def __str__(self):
         return f"POST: {self.id},  Author: {self.author}"
     
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.content[:20]}"
+
+
